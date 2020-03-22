675668407cbd2d4e20cb7163d914945c936d5aee
Fox Larsson <fox@artfox.dev>
2020-03-22 09:38:38 +0300

    Implemented stage 4/4
    Added calculations for differentiated payments
    Added overpay calculation
    Added argument parsing for terminal calls

--- creditcalc.py
+++ creditcalc.py
@@ -1,10 +1,73 @@
+import argparse
+import sys
 import math
 
+parser = argparse.ArgumentParser('\nCalculate your credit payments (differentiated or annuity.\n')
+parser.add_argument('--type', help='Type of payment: '
+                                   '"diff" for differentiated'
+                                   '"annuity" for annuity'
+                                   'Must be specified.')
+parser.add_argument('--payment', type=float, help='Monthly payment, only for annuity type.')
+parser.add_argument('--principal', type=float, help='Your credit principal')
+parser.add_argument('--periods', type=int, help='The number of months to repay your credit')
+parser.add_argument('--interest', type=float, help='Your credit interest'
+                                                   'Do not use a percent sign.'
+                                                   'Must to be specified.')
+args = parser.parse_args()
+param_error = 'Incorrect parameters'
+all_parameters = [args.payment, args.principal, args.periods, args.interest]
+type_values = ['annuity', 'diff']
 
-def calculate_count_of_months(prin, per_month, int_rate):
-    nominal_int_rate = int_rate / 12
-    periods = math.ceil(math.log(per_month / (per_month - nominal_int_rate * prin), 1 + nominal_int_rate))
-    num_years = int(periods // 12)
+
+def exit_on_param_error():
+    print(param_error)
+    sys.exit()
+
+
+def check_args():
+    if len(sys.argv) < 5:
+        exit_on_param_error()
+    elif args.type not in type_values:
+        exit_on_param_error()
+    elif args.type is None:
+        exit_on_param_error()
+    elif args.type == 'diff' and args.payment is not None:
+        exit_on_param_error()
+    elif args.interest is None:
+        exit_on_param_error()
+    else:
+        for param in all_parameters:
+            if param is not None:
+                if param < 0:
+                    exit_on_param_error()
+
+
+def calculate_diff_payment(interest, periods, principal):
+    total = 0
+    interest = interest / 12 / 100
+    for period in range(1, periods + 1):
+        period_payment = math.ceil(principal / periods + interest * (principal - (principal * (period - 1)) / periods))
+        total += period_payment
+        print(f'Month {period}: paid out {period_payment}')
+    overpayment = math.ceil(total - principal)
+    print(f'\nOverpayment = {overpayment}')
+
+
+def print_overpayment(payment, periods, principal):
+    overpayment = math.ceil(payment * periods - principal)
+    print(f'Overpayment = {overpayment}')
+
+
+def calculate_annuity_payment(principal, periods, interest):
+    payment = math.ceil(
+        principal * (interest * math.pow(1 + interest, periods) / (math.pow(1 + interest, periods) - 1)))
+    print(f'Your annuity payment = {payment}!')
+    print_overpayment(payment, periods, principal)
+
+
+def calculate_annuity_periods(principal, payment, interest):
+    periods = math.ceil(math.log(payment / (payment - interest * principal), 1 + interest))
+    num_years = math.ceil(periods // 12)
     num_months = math.ceil(periods % 12)
     if num_years and num_months:
         print(f'You need {num_years} years and {num_months} months to repay this credit!')
@@ -12,40 +75,26 @@ def calculate_count_of_months(prin, per_month, int_rate):
         print(f'You need {num_years} years to repay this credit!')
     elif num_months:
         print(f'You need {num_months} months to repay this credit!')
+    print_overpayment(payment, periods, principal)
 
 
-def calculate_annuity(prin, periods, int_rate):
-    nominal = int_rate / 12
-    annuity = prin * (nominal * math.pow(1 + nominal, periods) / (math.pow(1 + nominal, periods) - 1))
-    print(f'Your annuity payment = {math.ceil(annuity)}!')
-
-
-def calculate_principal(ann, periods, int_rate):
-    nominal = int_rate / 12
-    prin = ann / (nominal * math.pow(1 + nominal, periods) / (math.pow(1 + nominal, periods) - 1))
-    print(f'Your credit principal = {round(prin)}!')
+def calculate_annuity_principal(payment, periods, interest):
+    principal = payment / (interest * math.pow(1 + interest, periods) / (math.pow(1 + interest, periods) - 1))
+    print_overpayment(payment, periods, principal)
 
 
 def main():
-    action = input('What do you want to calculate?\n'      
-                   'type "n" - for count of months\n'      
-                   'type "a" for annuity monthly payment\n'
-                   'type "p - for credit principal:\n')
-    if action == 'n':
-        principal = float(input('Enter credit principal:\n'))
-        monthly = float(input('Enter monthly payment:\n'))
-        interest = float(input('Enter credit interest:\n')) / 100
-        calculate_count_of_months(principal, monthly, interest)
-    elif action == 'a':
-        principal = float(input('Enter credit principal:\n'))
-        num_periods = float(input('Enter count of periods:\n'))
-        interest = float(input('Enter credit interest:\n')) / 100
-        calculate_annuity(principal, num_periods, interest)
-    elif action == 'p':
-        monthly = float(input('Enter monthly payment:\n'))
-        num_periods = float(input('Enter count of periods:\n'))
-        interest = float(input('Enter credit interest:\n')) / 100
-        calculate_principal(monthly, num_periods, interest)
+    check_args()
+    if args.type == 'diff':
+        calculate_diff_payment(args.interest, args.periods, args.principal)
+    elif args.type == 'annuity':
+        args.interest = args.interest / 12 / 100
+        if args.payment is None:
+            calculate_annuity_payment(args.principal, args.periods, args.interest)
+        elif args.periods is None:
+            calculate_annuity_periods(args.principal, args.payment, args.interest)
+        elif args.principal is None:
+            calculate_annuity_principal(args.payment, args.periods, args.interest)
 
 
 main()
